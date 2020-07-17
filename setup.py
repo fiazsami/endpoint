bc9b8fa6a7b5d94a2ca403673ca03e2d5468c00f
TheDudeCodes <fiaz.sami@gmail.com>
2020-07-17 11:31:37 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="resourcer", # Replace with your own username
+    name="resourcer",
     version="0.1.5",
     author="Fiaz Sami",
     description="A simple wrapper around 'requests'",
